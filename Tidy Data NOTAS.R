c21f0a6e2b0a9c843444fc68047eb234adf94e07
CarlosGranadillo <70004367+CarlosGranadillo@users.noreply.github.com>
2020-08-21 11:38:28 -0500

    Update Tidy Data NOTAS.R

--- Tidy Data NOTAS.R
+++ Tidy Data NOTAS.R
@@ -1,14 +1,17 @@
-### Tidy Data - SOLUCIÓN ###
+### Tidy Data - SOLUCIÃ“N ###
 
 # Revisaremos una serie de datasets y en caso de no estar ordenados,
-# se le hará el tratamiento respectivo. 
+# se le harÃ¡ el tratamiento respectivo. 
 
 # Paquetes requeridos:
 library(tidyverse)
 library(datos)
 
+# Cambio:
+tabla2
+
 tabla4a
-# ¿Es un conjunto de datos ordenados?
+# Â¿Es un conjunto de datos ordenados?
 # NO. 
 # Para que el conjunto de datos sea ordenado los nombres de las columnas deben
 # corresponder a nombres de las variables, y en este caso los nombres de las 
@@ -16,73 +19,73 @@ tabla4a
 # Otra forma de ver el problema: Cada fila representa dos observaciones
 # en lugar de una.
 # A esto se le denomina: "Datos largos"
-# Solución: aplicar la función pivot_longer() a la base de datos.
+# SoluciÃ³n: aplicar la funciÃ³n pivot_longer() a la base de datos.
 tabla4a %>%
   pivot_longer(cols = c("1999","2000"), names_to = "anio", values_to = "casos")
-# Parámetros:
+# ParÃ¡metros:
 # cols -> nombres de las columnas que no son variables
 # names_to -> nombre de la variables cuyos VALORES forman los nombres de las
 # columnas (en este caso el nombre de esa variable es anio).
-# values_to -> nombre de la variables cuyos valores están repartidos por las
+# values_to -> nombre de la variables cuyos valores estÃ¡n repartidos por las
 # celdas
 
 
 tabla2
-# ¿Es un conjunto de datos ordenados?
+# Â¿Es un conjunto de datos ordenados?
 # NO. 
 # Para que el conjunto de datos sea ordenado cada fila debe ser una 
-# observación diferente, y en este caso vemos que las dos primeras filas son
-# en realidad una observación, la 3era y 4ta fila también son en realidad una 
-# sola observación y así.
-# Esto se soluciona usando la función pivot_wider().
+# observaciÃ³n diferente, y en este caso vemos que las dos primeras filas son
+# en realidad una observaciÃ³n, la 3era y 4ta fila tambiÃ©n son en realidad una 
+# sola observaciÃ³n y asÃ­.
+# Esto se soluciona usando la funciÃ³n pivot_wider().
 tabla2 %>%
   pivot_wider(names_from = tipo, values_from = cuenta)
-# Parámetros: 
+# ParÃ¡metros: 
 # names_from -> columna de la que se obtienen los nombres de las variables.
 # values_from -> columna de la que se obtienen los valores.
 
 
 tabla3
-# ¿Es un conjunto de datos ordenados?
+# Â¿Es un conjunto de datos ordenados?
 # NO.
 # Para que el conjunto de datos sea ordenado cada columna debe ser una
 # variable diferente, y en este caso se tiene una columna (tasa) que contiene
-# dos variables (casos y población).
-# Esto se soluciona usando la función separate().
+# dos variables (casos y poblaciÃ³n).
+# Esto se soluciona usando la funciÃ³n separate().
 tabla3 %>%
-  separate(col = tasa, into = c("casos","población"))
-# Los parámetros de la función separate() son:
-# La base de datos, en este caso no se especifica porque aquí está (señalar
+  separate(col = tasa, into = c("casos","poblaciÃ³n"))
+# Los parÃ¡metros de la funciÃ³n separate() son:
+# La base de datos, en este caso no se especifica porque aquÃ­ estÃ¡ (seÃ±alar
 # tabla3 antes del pipe).
 # col = el nombre de la columna que se quiere separar. Si simplemente se 
-# escribe el nombre de la columna R también lo entiende (no hace falta
+# escribe el nombre de la columna R tambiÃ©n lo entiende (no hace falta
 # escribir col= ).
-# into = c() -> nombre de las columna en las que se dividirá. El orden 
-# importa y la más a la izquierda en la primera.
-# Si el carácter que marca los dos valores que queremos separar -> 745 y
-# 19987071 en este caso -> NO es ni un número ni una letra, entonces no hay 
+# into = c() -> nombre de las columna en las que se dividirÃ¡. El orden 
+# importa y la mÃ¡s a la izquierda en la primera.
+# Si el carÃ¡cter que marca los dos valores que queremos separar -> 745 y
+# 19987071 en este caso -> NO es ni un nÃºmero ni una letra, entonces no hay 
 # que especificarlo, como en este caso.
 tabla3 %>%
   separate(tasa, into = c("casos", "poblacion"), sep = "/")
-# También se puede especificar el separador.
+# TambiÃ©n se puede especificar el separador.
 tabla3 %>%
   separate(tasa, into = c("casos", "poblacion"), convert = TRUE)
 # Si notamos la columna "tasa" que es la que queremos separar es de tipo 
-# caracter porque tiene el slash... ¿que sucede? La función separate() tiene 
+# caracter porque tiene el slash... Â¿que sucede? La funciÃ³n separate() tiene 
 # algo y es que al separar una columna R asume que su tipo es igual al tipo 
-# de columna original que se separó... pero vemos que son números, lo que se 
-# hace es poner el parámetro:
-# convert = TRUE -> convierte al tipo más adecuado las nuevas columnas.
+# de columna original que se separÃ³... pero vemos que son nÃºmeros, lo que se 
+# hace es poner el parÃ¡metro:
+# convert = TRUE -> convierte al tipo mÃ¡s adecuado las nuevas columnas.
 
 
 tabla5
-# Explicación
+# ExplicaciÃ³n
 # Esta es como una tabla aparte que simplemente propone el libro para explicar
-# otra función que no es tan utlizada pero que es bueno conocerla.
-# Vemos que es este data set se separa lo que sería un año completo en dos
+# otra funciÃ³n que no es tan utlizada pero que es bueno conocerla.
+# Vemos que es este data set se separa lo que serÃ­a un aÃ±o completo en dos
 # partes, una llamada "siglo" y otra que se llama "anio". 
 # Si queremos juntar estas dos columnas en una sola, puesto que en realidad
-# el año completo es una sola variable utilizamos la función -> unite()
+# el aÃ±o completo es una sola variable utilizamos la funciÃ³n -> unite()
 tabla5 %>%
   unite(anio_comp, siglo, anio)
 # Para que halla continuidad se define como separador dos comillas SIN espacio:
@@ -90,7 +93,7 @@ tabla5 %>%
   unite(anio_comp, siglo, anio, sep = "")
 
 ###
-# Hasta ahora todos los data sets que habiamos utilizado provenían de la 
+# Hasta ahora todos los data sets que habiamos utilizado provenÃ­an de la 
 # libreria "datos", pero para explicar el tema de valores faltantes usaremos
 # el siguiente data set escrito "a mano". Corranlo porfa...
 acciones <- tibble(
@@ -98,39 +101,39 @@ acciones <- tibble(
   trimestre = c(1, 2, 3, 4, 2, 3, 4),
   retorno = c(1.88, 0.59, 0.35, NA, 0.92, 0.17, 2.66))
 # Existen dos valores faltantes en este data set:
-# .	El retorno del cuarto trimestre de 2015 que está explícitamente perdido, 
+# .	El retorno del cuarto trimestre de 2015 que estÃ¡ explÃ­citamente perdido, 
 # debido a que la celda donde el valor debiera estar contiene NA.
-# .	El retorno del primer semestre de 2016 está implícitamente perdido, 
+# .	El retorno del primer semestre de 2016 estÃ¡ implÃ­citamente perdido, 
 # debido a que simplemente no aparece en el set de datos.
-# Inicialmente lo que se quiere es que todos los valores faltantes estén 
-# explícitos. 
+# Inicialmente lo que se quiere es que todos los valores faltantes estÃ©n 
+# explÃ­citos. 
 # Formas de hacer esto:
-# Primera: usando la función que vimos para corregir los datos "anchos".
+# Primera: usando la funciÃ³n que vimos para corregir los datos "anchos".
 # Puesto que que en esta forma de organizar los datos se genera una celda 
-# para cada trimestre de cada año.
+# para cada trimestre de cada aÃ±o.
 acciones %>%
   pivot_wider(names_from = anio, values_from = retorno)
 # La segunda forma de dejar los valores faltantes implicitos como explicitos
-# es usando la función spread.
+# es usando la funciÃ³n spread.
 acciones %>%
   spread(anio, retorno)
-# Los parámetros son:
+# Los parÃ¡metros son:
 # Lo que es el libro se llama como la llave o key = anio -> nombre de la 
 # variable cuyos valores formaran los nombres de las columnas.
-# value = retorno -> nombre de la variable cuyos valores están repartidos 
+# value = retorno -> nombre de la variable cuyos valores estÃ¡n repartidos 
 # por las celdas.
 acciones %>%
   complete(anio, trimestre)
 # Y la tercera forma de  pasar los valores faltantes implicitos como 
-# explicitos es usando la función complete(). Esta es la más recomendada puesto
-# Aquí los parámetros son nombres de columnas... lo que se hace es generar 
-# todas las combinaciones únicas... año 2015: semestre 1,2,3,4... y lo mismo 
-# con el año 2016.
-# La ventaja de usar esta es que además de ser muy fácil de entender, les
+# explicitos es usando la funciÃ³n complete(). Esta es la mÃ¡s recomendada puesto
+# AquÃ­ los parÃ¡metros son nombres de columnas... lo que se hace es generar 
+# todas las combinaciones Ãºnicas... aÃ±o 2015: semestre 1,2,3,4... y lo mismo 
+# con el aÃ±o 2016.
+# La ventaja de usar esta es que ademÃ¡s de ser muy fÃ¡cil de entender, les
 # queda el Data Set ordenado.
 
 # Ahora si lo que se quiere no es solo pasar los valores faltantes de 
-# implicitos a explicitos, sino que además se quiere remover las observaciones
+# implicitos a explicitos, sino que ademÃ¡s se quiere remover las observaciones
 # que tengan los NA, una forma de hacerlo es la siguiente:
 acciones %>%
   pivot_wider(names_from = anio, values_from = retorno) %>% 
@@ -140,10 +143,10 @@ acciones %>%
     values_to = "retorno", 
     values_drop_na = TRUE)
 # Primero se transforma todo a explicito y despues se lleva a una forma ordenada
-# el data set quitando todos los NA al agregar el parámetro values_drop_na)
+# el data set quitando todos los NA al agregar el parÃ¡metro values_drop_na)
 
 ###
-# Finalmente les pido que corran la última línea de código que es un último 
+# Finalmente les pido que corran la Ãºltima lÃ­nea de cÃ³digo que es un Ãºltimo 
 # data set donde explicaremos algo interesante.
 tratamiento <- tribble(
   ~sujeto, ~tratamiento, ~respuesta,
@@ -154,10 +157,10 @@ tratamiento <- tribble(
 # En algunos casos en que la fuente de datos se ha usado principalmente para 
 # ingresar datos, los valores faltantes indican que el valor previo debe 
 # arrastrarse hacia adelante:
-# Solución:
-# Puedes completar los valores faltantes usando fill(). Esta función toma un 
+# SoluciÃ³n:
+# Puedes completar los valores faltantes usando fill(). Esta funciÃ³n toma un 
 # conjunto de columnas sobre las cuales los valores faltantes son reemplazados 
-# por el valor anterior más cercano que se haya reportado.
+# por el valor anterior mÃ¡s cercano que se haya reportado.
 tratamiento %>%
   fill(sujeto)
 
@@ -168,7 +171,7 @@ tratamiento <- tribble(
   NA, 2, 10,
   NA, 3, 9,
   "Katherine Burke", 1, 4, NA, 3, 7)
-# Solución:
+# SoluciÃ³n:
 tratamiento %>%
   fill(sujeto)
 
